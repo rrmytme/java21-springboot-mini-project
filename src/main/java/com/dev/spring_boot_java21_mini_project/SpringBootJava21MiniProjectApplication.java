813adfe16106fc6d8c0bc5676212a604f85e53c5
rrmytme <rrmytme@gmail.com>
2025-05-25 19:00:47 +1200

    code cleanup

---
+++ src/main/java/com/dev/spring_boot_java21_mini_project/SpringBootJava21MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_java21_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava21MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJava21MiniProjectApplication.class, args);
+	}
+
+}
